839558da291bcb3422bc8bd33136f6241bfa8d92
Nguyen Hieu <manhhiu0509@gmail.com>
2021-06-21 09:41:20 +0700

    covid app

---
+++ Covid-app/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Covid-app
+//
+//  Created by NGUYEN MANH HIEU on 20/06/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
